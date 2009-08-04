772f871cb0595de642d2795021a191a5e0d44cd1
Xerxes Battiwalla <xerxesb@gmail.com>
2009-08-04 14:24:04 +1000

    Implemented Events for Alarmclock

--- prep/exercise_to_complete_for_day_1/product/nothinbutdotnetprep/events/AlarmClock.cs
+++ prep/exercise_to_complete_for_day_1/product/nothinbutdotnetprep/events/AlarmClock.cs
@@ -1,11 +1,37 @@
 using System;
+using System.Threading;
 
 namespace nothinbutdotnetprep.events
 {
     public class AlarmClock
     {
-       public void start() {
-       }
+        readonly int _ringAfterMilliseconds;
+
+        public AlarmClock(int ring_after_milliseconds)
+        {
+            _ringAfterMilliseconds = ring_after_milliseconds;
+        }
+
+        public void start()
+        {
+            OnRing();
+        }
+
+        private void OnRing()
+        {
+            var temp_alarm_event = ring;
+            if (temp_alarm_event != null) 
+            {
+                temp_alarm_event(this, new AlarmRingEventArgs 
+                {
+                    time_the_alarm_was_started = DateTime.Now, 
+                    time_the_alarm_rang = DateTime.Now.AddMilliseconds(_ringAfterMilliseconds)
+                });
+            }
+        }
+
+        public delegate AlarmRingEventArgs AlarmRingDelegate(object sender, AlarmRingEventArgs e);
+        public event AlarmRingDelegate ring;
 
     }
 }
