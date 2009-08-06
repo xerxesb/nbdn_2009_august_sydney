28deede76d3afb942eac08312a7728c7b51990d9
Jean-Paul S. Boodhoo <jp@jpboodhoo.com>
2009-08-06 10:24:18 +1000

    Adding the first start to the BasicContainer specs

---
+++ store/product/nothinbutdotnetstore.tests/infrastructure/BasicContainerSpecs.cs
@@ -0,0 +1,40 @@
+using System.Data;
+using System.Data.SqlClient;
+using developwithpassion.bdd.contexts;
+using developwithpassion.bdd.mbunit;
+using developwithpassion.bdd.mbunit.standard.observations;
+using developwithpassion.bdddoc.core;
+using developwithpassion.commons.core.infrastructure.containers;
+using nothinbutdotnetstore.infrastructure.containers.basic;
+
+namespace nothinbutdotnetstore.tests.infrastructure
+{
+    public class BasicContainerSpecs
+    {
+        public abstract class concern : observations_for_a_sut_with_a_contract<Container,
+                                            BasicContainer> {}
+
+        [Concern(typeof (BasicContainer))]
+        public class when_getting_an_instance_of_a_component : concern
+        {
+            context c = () => {
+            
+
+            };
+
+            because b = () => {
+
+                result = sut.instance_of<IDbConnection>();
+            };
+
+
+            it should_return_the_instance_that_was_resolved_using_the_types_dependency_resolver = () =>
+            {
+                result.should_be_equal_to(sql_connection);
+            };
+
+            static IDbConnection result;
+            static SqlConnection sql_connection;
+        }
+    }
+}
